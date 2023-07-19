d18bb88a17b9493298b08db7cd852be9ad75e86a
Riccardo Mancini <riccardo.mancini01@gmail.com>
2023-07-19 04:21:06 +0200

    Better auto bracket closer for nvim

--- .config/nvim/after/plugin/autoclose.lua
+++ .config/nvim/after/plugin/autoclose.lua
@@ -1,1 +1,8 @@
-require("autoclose").setup()
+--require("autoclose").setup()
+
+-- for autopairs:
+require("nvim-autopairs").setup({
+    check_ts = true,
+    ignored_next_char = "[%w%.]",
+    fast_wrap = {}, -- press alt-e while in this position (|)asfd to wrap word in parentheses
+})
--- .config/nvim/lua/jelly/packer.lua
+++ .config/nvim/lua/jelly/packer.lua
@@ -13,7 +13,10 @@ return require('packer').startup(function(use)
         requires = { {'nvim-lua/plenary.nvim'} }
     }
 
-    use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+    use{
+        'nvim-treesitter/nvim-treesitter',
+        run = ':TSUpdate'
+    }
     use 'nvim-treesitter/playground'
     use 'navarasu/onedark.nvim'
     use 'theprimeagen/harpoon'
@@ -49,5 +52,6 @@ return require('packer').startup(function(use)
     }
 
     use 'nvim-tree/nvim-tree.lua'
-    use 'm4xshen/autoclose.nvim'
+    --use 'm4xshen/autoclose.nvim'
+    use 'windwp/nvim-autopairs'
 end)
