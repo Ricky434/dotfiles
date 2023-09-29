6d7117534fa9c79923acc69fc8dac61a088385c5
Riccardo Mancini <riccardo.mancini01@gmail.com>
2023-09-29 14:33:48 +0200

    Nvim: fix paste, add format on save for go

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -32,6 +32,7 @@ lsp.on_attach(function(client, bufnr)
   local opts = {buffer = bufnr, remap = false}
 
   vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+  vim.keymap.set("n", "gi", function() vim.lsp.buf.implementation() end, opts)
   vim.keymap.set("n", "gl", function() vim.diagnostic.open_float() end, opts)
   vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
   vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
@@ -42,6 +43,16 @@ lsp.on_attach(function(client, bufnr)
   vim.keymap.set('n', 'gr', '<cmd>Telescope lsp_references<cr>', {buffer = true}) -- remap to use telescope
   vim.keymap.set("n", "<leader>vws", '<cmd>Telescope lsp_dynamic_workspace_symbols<cr>', {buffer = true}) -- remap to use telescope
 end)
+
+lsp.format_on_save({
+    servers = {
+        ['gopls'] = {'go'},
+    }
+})
+
+lsp.configure('html', {
+    filetypes = { "html", "template" }
+})
 
 lsp.setup()
 
