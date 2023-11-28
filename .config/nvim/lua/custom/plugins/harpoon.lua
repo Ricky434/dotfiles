baf3929fa7f28ddb5e842cac796b64d8db9092b2
Riccardo Mancini <riccardo.mancini01@gmail.com>
2023-11-28 22:08:26 +0100

    Update nvim config

--- .config/nvim/lua/custom/plugins/harpoon.lua
+++ .config/nvim/lua/custom/plugins/harpoon.lua
@@ -4,11 +4,11 @@ return {
   dependencies = {
     "nvim-lua/plenary.nvim",
   },
-  config = function ()
+  config = function()
     local mark = require("harpoon.mark")
     local ui = require("harpoon.ui")
 
-    vim.keymap.set("n", "<leader>a", mark.add_file)
+    vim.keymap.set("n", "<leader>h", mark.add_file, { desc = 'Add file to harpoon' })
     vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
 
     vim.keymap.set("n", "<C-h>", function() ui.nav_file(1) end)
