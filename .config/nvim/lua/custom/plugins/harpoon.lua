970b2b071765e4975024de3b1fb4e23c4f163af4
Riccardo Mancini <riccardo.mancini01@gmail.com>
2024-02-08 15:26:13 +0100

    Nvim use haskell-tools, add harpoon keybinds

--- .config/nvim/lua/custom/plugins/harpoon.lua
+++ .config/nvim/lua/custom/plugins/harpoon.lua
@@ -8,15 +8,18 @@ return {
   config = function()
     local harpoon = require("harpoon")
 
-    harpoon:setup()
+    harpoon:setup({})
 
     vim.keymap.set("n", "<leader>h", function() harpoon:list():append() end, { desc = 'Add file to harpoon' })
     vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
     vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
     vim.keymap.set("n", "<C-j>", function() harpoon:list():select(2) end)
-    vim.keymap.set("n", "<C-k>", function() harpoon:list():select(3) end)
-    vim.keymap.set("n", "<C-l>", function() harpoon:list():select(4) end)
+    vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
+    vim.keymap.set("n", "<C-m>", function() harpoon:list():select(4) end)
+
+    vim.keymap.set("n", "<leader>o", function() harpoon:list():prev() end, { desc = 'Go to prev in harpoon list' })
+    vim.keymap.set("n", "<leader>i", function() harpoon:list():next() end, { desc = 'Go to next in harpoon list' })
   end,
 }
 
