e4cc43229c55f61d2d6b2c5438b7f830fea63757
Riccardo Mancini <riccardo.mancini01@gmail.com>
2024-04-01 00:07:05 +0200

    Add todo and surround brackets plugins to neovim
    Change how Enter works in nvim-cmp to avoid accidental confirmation of
    suggestion, add todo highlighting plugin, add plugins to more easily
    add/remove brackets surrounding words and moving around them

---
+++ .config/nvim/lua/custom/plugins/todo-comments.lua
@@ -0,0 +1,25 @@
+return {
+  "folke/todo-comments.nvim",
+  dependencies = { "nvim-lua/plenary.nvim", "nvim-treesitter/nvim-treesitter" },
+  config = function()
+    require('todo-comments').setup {
+      signs = false,
+    }
+
+    vim.keymap.set("n", "]t", function()
+      require("todo-comments").jump_next()
+    end, { desc = "Next todo comment" })
+
+    vim.keymap.set("n", "[t", function()
+      require("todo-comments").jump_prev()
+    end, { desc = "Previous todo comment" })
+  end
+}
+
+-- FIX: fix
+-- TODO: todo
+-- HACK: hack
+--WARN: warn
+-- PERF: perf
+-- NOTE: note
+-- TEST: test
