da06e4a007db0eac489512b59819fa422977233a
Riccardo Mancini <riccardo.mancini01@gmail.com>
2024-10-01 19:13:52 +0200

    Nvim: add trouble, change folding symbols

--- .config/nvim/lua/custom/plugins/telescope.lua
+++ .config/nvim/lua/custom/plugins/telescope.lua
@@ -18,10 +18,21 @@ return {
         },
     },
     config = function()
+        -- These use the Trouble plugin
+        local open_with_trouble = require("trouble.sources.telescope").open
+        -- Use this to add more results without clearing the trouble list
+        local add_to_trouble = require("trouble.sources.telescope").add
+
         require('telescope').setup {
             defaults = {
                 mappings = {
                     i = {
+                        ["<c-t>"] = open_with_trouble,
+                        ["<m-t>"] = add_to_trouble
+                    },
+                    n = {
+                        ["<c-t>"] = open_with_trouble,
+                        ["<m-t>"] = add_to_trouble
                     },
                 },
             },
